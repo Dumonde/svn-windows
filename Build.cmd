96d68868e0e96348730e1bfec390ffcb5bc53aa5
Dumonde <dumonde@bk.ru>
2023-11-28 13:38:17 +0500

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,10 +1,7 @@
 echo build.cmd
 
 if "%vcversion%"=="vs17" if "%platform%"=="x64" call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
 if "%vcversion%"=="vs16" if "%platform%"=="x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-if "%vcversion%"=="vc15" if "%platform%"=="x64" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 dir \downloads
 if exist \downloads\zlib-%zlib_version% dir \downloads\zlib-%zlib_version%
@@ -23,7 +20,7 @@ cd \downloads\subversion-%svn_version%
 
 if "%run_build%"=="no" exit 0
 
-echo C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2022 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.10
-C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2022 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.10
+echo C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.10
+C:\Python27-x64\python gen-make.py --release -t vcproj --vsnet-version=2017 --with-apr-util=C:\Apache24 --with-apr=C:\Apache24 --with-apr-iconv=C:\Apache24 --with-apr_memcache=C:\Apache24 --with-httpd=C:\Apache24 --with-openssl=C:\OpenSSL-v111-Win64 --with-zlib=C:\Apache24 --with-sqlite=C:\downloads\sqlite-amalgamation-%sqlite_version% --with-jdk="C:\Program Files\Java\jdk9" --with-serf=\downloads\serf-1.3.10
 if "%run_clean%"=="yes" devenv subversion_vcnet.sln /Clean "Release|x64" || exit 0
 devenv subversion_vcnet.sln /Build "Release|x64" || exit 0
